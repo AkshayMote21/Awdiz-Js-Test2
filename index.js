6966ef774819ca5b0a7c57256dbdd86f13fdafa1
Akshayy <mote.akshay.18et1011@gmail.com>
2024-05-22 20:25:52 +0530

    updated..

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // test - 2
-// Question 1:
-// Write a JavaScript function that takes an array of numbers as input and returns a new array with each number squared.
+// Question 1: Write a JavaScript function that takes an array of numbers as input and returns a new array with each number squared.
 // Input: [1, 2, 3, 4, 5]
+
 // var array1 = [1,2,3,4,5];
 // function Squared(array){
 //     var array2 = [];
@@ -12,11 +12,14 @@
 // }
 // console.log(Squared(array1));
 
+//---------------------------------------------------------------------------------------------------------------
 // Question 2:
 // Create a JavaScript function that checks if a given string is a palindrome (reads the same forwards and backwards).
 
 // Input: "madam"
 
+
+//---------------------------------------------------------------------------------------------------------------
 // Question 3:
 // Write a JavaScript function that takes two arrays as input and returns a new array that contains only the elements that are present in both arrays (intersection).
 
@@ -35,17 +38,22 @@
 //     return newArray;
 // }
 // console.log(Intersection(array1,array2));
+
+
+//---------------------------------------------------------------------------------------------------------------
 // Question 4:
 // Create a JavaScript function that takes an object and a key as input, and returns the value associated with that key in the object. If the key does not exist, return a default value.
 // Input: {name: "Alice", age: 25}, "name"
-// var obj ={name : "Alice", age:25}
 
+// var obj ={name : "Alice", age:25}
 // function Value(obj){
 //     console.log(obj.name);
 //     console.log(obj.age);
 // }
-
 // Value(obj);
+
+
+//---------------------------------------------------------------------------------------------------------------
 // Question 5:
 // Write a JavaScript function that takes a number as input and returns an array of its digits.
 // Input: 12345
@@ -56,25 +64,42 @@
     
 // }
 // console.log(ArrayOfDigits(number));
+
+
+//---------------------------------------------------------------------------------------------------------------
 // Question 6:
 // Create a JavaScript function that takes a string as input and returns a new string with each character duplicated (e.g., "hello" becomes "hheelllloo").
 
 // Input: "hello"
+function duplicateLetters(str){
+        var str2='';
+    for(var i=0;i<str.length;i++){     
+        str2 += str[i]+str[i];
+    }
+    return str2;
+}
+console.log(duplicateLetters("hello"));
 
+//---------------------------------------------------------------------------------------------------------------
 // Question 7:
 // Write a JavaScript function that takes an array of objects and a key, and returns an array containing the values associated with that key from each object.
 
 // Input: [{name: "Alice"}, {name: "Bob"}, {name: "Charlie"}], "name"
 
+
+//---------------------------------------------------------------------------------------------------------------
 // Question 8:
 // Create a JavaScript function that takes a sentence as input and returns the number of words in the sentence.
 
 // Input: "This is a sample sentence."
-var str = "This is a sample sentence.";
-function NumberOfWords(str){
+// var str = "This is a sample sentence.";
+// function NumberOfWords(str){
     
-}
-console.log(NumberOfWords(str));
+// }
+// console.log(NumberOfWords(str));
+
+
+//---------------------------------------------------------------------------------------------------------------
 // Question 9:
 // Write a JavaScript function that takes an array of numbers and returns the second smallest number in the array.
 // Input: [10, 2, 14, 5, 7]
@@ -90,6 +115,9 @@ console.log(NumberOfWords(str));
 //     // return array[i];
 // }
 // console.log(SecondSmallestNumber(array));
+
+
+//---------------------------------------------------------------------------------------------------------------
 // Question 10:
 // Create a JavaScript function that takes a date object and returns a formatted string in the format "YYYY-MM-DD".
 
